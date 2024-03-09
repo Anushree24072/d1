6f18adf0fbcb17992ac93edc115d71ff9c8ca346
Anushree.V <150107145+Anushree24072@users.noreply.github.com>
2024-03-09 17:30:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#printing
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+# /n
+print("hello world /n hello world")
+#/tab
+print("hello" + " " + "anu")
+
+#input
+input("what is yr name")
